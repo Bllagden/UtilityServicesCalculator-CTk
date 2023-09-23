2f43f365542231197abe6eccd61d8a79f8ef0703
Bllagden <bllagden99@gmail.com>
2023-09-24 03:39:15 +0400

    Refactor ItemOperations: => abstract class

--- src/model/operations/item_opers/item_operations.py
+++ src/model/operations/item_opers/item_operations.py
@@ -1,5 +1,8 @@
-class ItemOperations:
-    """The base class for working with items in a DB.
+from abc import ABC, abstractmethod
+
+
+class ItemOperations(ABC):
+    """The abstract class for working with items in a DB.
     Items: Houses, Tariffs, Years, Meters.
     The items are stored in the DB in the appropriate tables.
     Submitting non-DB items raises an exception."""
@@ -22,17 +25,17 @@ class ItemOperations:
         else:
             raise ValueError("The item was not found in the DB.")
 
+    @abstractmethod
     def _is_item_in_db(self):
-        """Checking if an element exists in the DB.
-        Must be overridden in child classes."""
+        """Checking if an element exists in the DB."""
         pass
 
+    @abstractmethod
     def _add_into_db(self):
-        """Adding an element to the DB.
-        Must be overridden in child classes."""
+        """Adding an element to the DB."""
         pass
 
+    @abstractmethod
     def _del_from_db(self):
-        """Deleting an item from the DB.
-        Must be overridden in child classes."""
+        """Deleting an item from the DB."""
         pass
