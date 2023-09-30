6444a2641bb9f2ec5c99e9e8c1c5542f43d38a20
Bllagden <bllagden99@gmail.com>
2023-09-30 21:43:20 +0400

    Docstring for db_settings

--- src/model/database/db_settings.py
+++ src/model/database/db_settings.py
@@ -1,3 +1,7 @@
+"""
+NON-DEFAULT.db is used when main.py runs from the root directory of the project
+(UtilityServicesCalculator-CTk), not from src.
+"""
 DB_DEFAULT_PATH = "model/database/"
 DB_NON_DEFAULT_PATH = ""
 DB_DEFAULT_NAME = "DEFAULT.db"
